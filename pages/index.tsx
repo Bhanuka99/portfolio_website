bd202cbe456e6eb9f8292e8c8855207ee63bc11b
Bhanuka99 <bhanuka.ariyarathne@gmail.com>
2024-12-02 11:08:55 +0530

    2024/12/01

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,12 +24,12 @@ export default function Home() {
         </style>
       </Head>
       <div>
-        <main className="font-poppins bg-blue-50 min-h-screen px-10">
+        <main className="font-poppins bg-blue-50 min-h-screen px-10 md:px-20 lg:px-40">
           {/* nav and hero */}
           <section>
             {/* nav */}
             <nav className="py-10 flex justify-between">
-              <h1 className="text-sm text-blue-400">Developed by Bhanuka Ariyarathne</h1>
+              <h1 className="text-sm md:text-xl text-blue-400">Developed by Bhanuka Ariyarathne</h1>
               <ul className="flex items-center">
                 {/* icon */}
                 <li>
@@ -45,10 +45,10 @@ export default function Home() {
               </ul>
             </nav>
             {/* hero */}
-            <div className="text-center p-10">
-              <h1 className=" text-5xl font-bold text-blue-400">Bhanuka Ariyarathne</h1>
+            <div className="text-center">
+              <h1 className=" text-5xl md:text-5xl lg:text-7xl font-bold text-blue-400">Bhanuka Ariyarathne</h1>
               <h3 className=" text-2xl py-4">UNDERGRADUATE | BACHELOR OF INFORMATION TECHNOLOGY </h3>
-              <p className="text-sm py-4 text-gray-400 leading-6">A final year proactive Bachelor of Information Technology (External) student at the University of Colombo School of Computing. Possess strong communication skills with fluency in English and Sinhala. Keen to pursue a career in professional services with a focus on IT and software development roles.</p>
+              <p className="text-sm py-4 text-gray-400 leading-6 max-w-xl mx-auto md:text-md">A final year proactive Bachelor of Information Technology (External) student at the University of Colombo School of Computing. Possess strong communication skills with fluency in English and Sinhala. Keen to pursue a career in professional services with a focus on IT and software development roles.</p>
             </div>
             {/* social icons */}
             <div className="text-5xl flex justify-center gap-16 text-gray-500">
@@ -65,10 +65,11 @@ export default function Home() {
           <section>
             <div>
               <h2 className="opacity-60 text-4xl py-8 font-bold">About Me</h2>
-              <p className="text-sm text-gray-400 leading-6"> Currently i am doing my Bachelors and i have a Higher Diploma and a Diploma in Information Tecnologies in University of Colombo. Those are my main education qualifications. I also have a 3 years of working experience as a Production Manager in D.D.Atukorala Group (pvt)ltd.</p>
+              <p className="text-sm text-gray-400 leading-6"> Currently i am doing my Bachelors and i have a Higher Diploma and a Diploma in Information Tecnology in University of Colombo. Those are my main education qualifications. I also have a 3 years of working experience as a Production Manager in D.D.Atukorala Group (pvt)ltd.</p>
               <h2 className="opacity-60 text-4xl py-8 font-bold">Skills</h2>
             </div>
             {/* skill cards */}
+            <div className="md:flex lg:flex gap-10 mx-auto">
             <div className="bg-blue-300 text-center shadow-sm p-5 rounded-xl my-10">
             <Image src={langskill}
              alt="lang pic"
@@ -95,6 +96,7 @@ export default function Home() {
              className="inline"/>
              <h2 className="text-xl font-bold">Others</h2>
              <p className="text-sm py-2 text-gray-800 leading-6"> Currently i am doing my Bachelors and i have a Higher Diploma and a Diploma in Information Tecnologies in University of Colombo.</p>
+            </div>
             </div>
           </section>
         </main>
